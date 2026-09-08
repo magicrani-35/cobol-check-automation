cda7b6f680929613a722c0e736631fa44223eabe
Barbara <babs2818@icloud.com>
2026-09-08 05:43:28 -0400

    Update COBOL check command in mainframe_operations.sh

--- .github/scripts/mainframe_operations.sh
+++ .github/scripts/mainframe_operations.sh
@@ -17,7 +17,7 @@ echo "Remote COBOL Check directory:
 $COBOL_CHECK_DIRECTORY"
 
 zowe zos-ssh issue command \
-  "java -jar bin/cobol-check-0.2.19.jar --programs NUMBERS && ls -l 'testruns/NUMBERS.JCL'" \
+  "java -jar bin/cobol-check-0.2.19.jar --programs NUMBERS && ls -l 'testruns/CC##99.CBL'" \
   --cwd "$COBOL_CHECK_DIRECTORY" \
   --host "$ZOWE_HOST" \
   --port 22 \
